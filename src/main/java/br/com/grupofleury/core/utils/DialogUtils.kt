127bfe82bb3315313cfe8976deb8f6358a5638c7
Ricardo Sousa <sousa.ricardorodrigues@gmail.com>
2020-05-14 11:15:02 -0300

    adding simple dialog to tests

---
+++ src/main/java/br/com/grupofleury/core/utils/DialogUtils.kt
@@ -0,0 +1,27 @@
+package br.com.grupofleury.core.utils
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import timber.log.Timber
+
+fun BasicDialog(context: Context, title: String, msg: String) {
+    val builder = AlertDialog.Builder(context)
+
+    builder.setTitle(title)
+    builder.setMessage(msg)
+
+    builder.setPositiveButton("OK"){dialog, which ->
+        Timber.d("Ok, we change the app background.")
+    }
+//
+//    builder.setNegativeButton("No"){dialog,which ->
+//        Timber.d("You are not agree.")
+//    }
+//
+//    builder.setNeutralButton("Cancel"){_,_ ->
+//        Timber.d("You cancelled the dialog.")
+//    }
+
+    val dialog: AlertDialog = builder.create()
+    dialog.show()
+}
